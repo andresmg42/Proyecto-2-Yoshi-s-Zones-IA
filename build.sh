a66c11ef8b58d73dac8a6f645e98a533a643bd61
andres david ortega arteaga <89621541+andresmg42@users.noreply.github.com>
2025-07-11 16:58:38 -0500

    Create build.sh

---
+++ build.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+git clone https://github.com/andresmg42/Proyecto-2-Yoshi-s-Zones-IA.git 
+
+cd Proyecto-2-Yoshi-s-Zones-IA
+
+python -m venv .venv 
+
+source .venv/bin/activate
+
+pip install -r requirements.txt
+
+python runner.py
